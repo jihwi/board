b1cf2fb0c36f9424286ae88e1897a3b2ebd88ab4
김지휘 <jihwi.kim@cj.net>
2022-07-07 11:36:43 +0900

    [김지휘]2022-07-07 dbConfig 통합

--- src/main/java/com/hbproject/board/config/MybatisConfig.java
+++ src/main/java/com/hbproject/board/config/MybatisConfig.java
@@ -3,6 +3,8 @@ package com.hbproject.board.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -15,6 +17,13 @@ import javax.sql.DataSource;
 //com.hbproject.board.**.mapper와 동일
 @MapperScan("com.hbproject.board.services.*.mapper")
 public class MybatisConfig {
+
+    //@Value 는 단일값 주입이기때문에 여러값을 받아올때는 @ConfigurationProperties을 이용해야한다.
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    @Bean
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
