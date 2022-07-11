10877fee14942ca47bb6cb19b7e6a03caabe6d9d
김지휘 <jihwi.kim@cj.net>
2022-07-11 15:49:37 +0900

    [김지휘]2022-07-11 jar 파일 생성 테스트

--- src/main/java/com/hbproject/board/BoardApplication.java
+++ src/main/java/com/hbproject/board/BoardApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class, args);
+        System.out.println("HELLO, WORLD");
+    }
 
 }
