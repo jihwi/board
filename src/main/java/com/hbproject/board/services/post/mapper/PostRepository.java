def375acbb7572859589167a1c7c5da6a796a483
김지휘 <jihwi.kim@cj.net>
2022-07-11 17:38:47 +0900

    [김지휘]2022-07-11 post crud 작업

--- src/main/java/com/hbproject/board/services/post/mapper/PostRepository.java
+++ src/main/java/com/hbproject/board/services/post/mapper/PostRepository.java
@@ -1,11 +1,47 @@
 package com.hbproject.board.services.post.mapper;
 
 import com.hbproject.board.services.post.dto.Post;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository {
+    /**
+     * Post 리스트 조회
+     *
+     * @return
+     */
     List<Post> selectAllList();
+
+    /**
+     * Post 단건 조회
+     *
+     * @param postId
+     * @return
+     */
+    Post selectPost(@Param("postId") int postId);
+
+    /**
+     * Post 등록
+     *
+     * @param post
+     */
+    void insertPost(Post post);
+
+    /**
+     * Post 수정
+     *
+     * @param postId
+     * @param post
+     */
+    void updatePost(@Param("postId") int postId, Post post);
+
+    /**
+     * Post 삭제
+     *
+     * @param postId
+     */
+    void deletePost(int postId);
 }
--- src/main/java/com/hbproject/board/services/post/service/PostService.java
+++ src/main/java/com/hbproject/board/services/post/service/PostService.java
@@ -7,12 +7,58 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Post CURD
+ */
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
 
+    /**
+     * Post 리스트 조회
+     *
+     * @return
+     */
     public List<Post> getAllPostList() {
         return postRepository.selectAllList();
+    }
+
+    /**
+     * Post 단건 조회
+     *
+     * @param postId
+     * @return
+     */
+    public Post getPost(int postId) {
+        return postRepository.selectPost(postId);
+    }
+
+    /**
+     * Post 등록
+     *
+     * @param post
+     */
+    public void registerPost(Post post) {
+        postRepository.insertPost(post);
+    }
+
+    /**
+     * Post 수정
+     *
+     * @param postId
+     * @param post
+     */
+    public void modifyPost(int postId, Post post) {
+        postRepository.updatePost(postId, post);
+    }
+
+    /**
+     * Post 삭제
+     *
+     * @param postId
+     */
+    public void deletePost(int postId) {
+        postRepository.deletePost(postId);
     }
 }
